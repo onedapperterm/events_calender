e311eb3db6fd56534b445f614d3b37a64a2ab9c8
Julian <jmcastrol@unal.edu.co>
2023-03-22 10:27:38 +0100

    delete of non found components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EventComponent } from "./event/event.component";
-import { AllEventsComponent } from "./all-events/all-events.component";
-import { ContactComponent } from "./contact/contact.component";
 
 const routes: Routes = [
   { path: 'home', component: EventComponent },
-  { path: 'all_events', component: AllEventsComponent },
-  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
